dbfb9d2eefff4c8ed62d75707110506a763d4ed4
Abhinav <abhiks3112@gmail.com>
2020-11-08 00:02:05 +0530

    SqlDataAdapter and Datasets concepts shown with one table and multiple tables at the same time

---
+++ WebApplication/ADODOTNET_Concepts_WebApplication/ADODOTNET_Concepts_WebApplication/DataSetWebForm.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DataSetWebForm.aspx.cs" Inherits="ADODOTNET_Concepts_WebApplication.DataSetWebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btnLoadData" runat="server" Text="Load Data" OnClick="btnLoadData_Click"/>
+            <asp:Button ID="btnClearCache" runat="server" Text="Clear Cache" OnClick="btnClearCache_Click"/>
+        </div>
+        <br />
+        <br />
+         <div>
+            <asp:Label ID="lblStatus" runat="server"></asp:Label>
+        </div>
+        <br />
+        <br />
+        <div>
+            <asp:GridView ID="gvProductInventory" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+                <AlternatingRowStyle BackColor="White" />
+                <EditRowStyle BackColor="#7C6F57" />
+                <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#E3EAEB" />
+                <SelectedRowStyle BackColor="#C5BBAF" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#F8FAFA" />
+                <SortedAscendingHeaderStyle BackColor="#246B61" />
+                <SortedDescendingCellStyle BackColor="#D4DFE1" />
+                <SortedDescendingHeaderStyle BackColor="#15524A" />
+            </asp:GridView>
+            <br />
+            <br />
+            <asp:GridView ID="gvProductCategories" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+                <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+                <EditRowStyle BackColor="#999999" />
+                <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+                <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#E9E7E2" />
+                <SortedAscendingHeaderStyle BackColor="#506C8C" />
+                <SortedDescendingCellStyle BackColor="#FFFDF8" />
+                <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication/ADODOTNET_Concepts_WebApplication/ADODOTNET_Concepts_WebApplication/DataSetWebForm.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ADODOTNET_Concepts_WebApplication
+{
+    public partial class DataSetWebForm : System.Web.UI.Page
+    {
+        string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btnLoadData_Click(object sender, EventArgs e)
+        {
+            if (Cache["Data"] == null)
+            {
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    // Fetching multiple tables at the same time
+                    SqlDataAdapter da = new SqlDataAdapter("spGetProductAndCategoriesData", con);
+                    // Another way without using constructor
+                    // da.SelectCommand = new SqlCommand("spGetProductAndCategoriesData", con);
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                    // To pass any value as paramter
+                    // da.SelectCommand.Parameters.AddWithValue("@ProductID", Convert.ToInt32(txtProductId.Text));
+
+                    DataSet ds = new DataSet();
+                    /*
+                     *  Unlike data reader, data adapter does not need connection opened explicitly before executing Fill,
+                     *  it automatically opens connection using this statement whenever needed and closes it immediately
+                     *  as soon as the operation is finished.
+                     */
+                    da.Fill(ds);
+
+                    // Loading data in cache
+                    Cache["Data"] = ds;
+
+                    AssignDataSourceToGridViews(ds);
+                    lblStatus.Text = "Data loaded from database.";
+                }
+            }
+            else
+            {
+                // Loading data from cache
+                DataSet ds = (DataSet)Cache["Data"];
+                AssignDataSourceToGridViews(ds);
+                lblStatus.Text = "Data loaded from cache.";
+            }
+        }
+
+        private void AssignDataSourceToGridViews(DataSet ds)
+        {
+            // We can give name to datasets/table fetched from database for better reading using Table collection inside dataset
+            ds.Tables[0].TableName = "Inventory";
+            ds.Tables[1].TableName = "Categories";
+
+            // We can select table either using index or name given by us or default name assigned eg. Table, Table1, etc
+            gvProductInventory.DataSource = ds.Tables["Inventory"];
+            gvProductInventory.DataBind();
+
+            gvProductCategories.DataSource = ds.Tables["Categories"];
+            gvProductCategories.DataBind();
+        }
+
+        protected void btnClearCache_Click(object sender, EventArgs e)
+        {
+            if(Cache["Data"] != null)
+            {
+                // Removing data from cache
+                Cache.Remove("Data");
+                lblStatus.Text = "Data cleared from cache.";
+            }
+            else
+            {
+                lblStatus.Text = "No data left to remove from cache.";
+            }
+        }
+    }
+}
